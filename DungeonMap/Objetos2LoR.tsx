3d814f741a476b6b5f9cd37549860000dbbdf37f
AnaRodriguesESG <65546790+AnaRodriguesESG@users.noreply.github.com>
2022-05-26 17:59:45 +0100

    Criação do mapa da dungeon

---
+++ DungeonMap/Objetos2LoR.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="Objetos2LoR" tilewidth="16" tileheight="16" tilecount="512" columns="32">
+ <image source="assets/atlas_16x.png" width="512" height="256"/>
+</tileset>
