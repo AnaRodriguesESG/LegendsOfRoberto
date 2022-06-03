c7aee62422716b6c14c84dfe611aa1be9074f298
AnaRodriguesESG <65546790+AnaRodriguesESG@users.noreply.github.com>
2022-06-03 14:48:57 +0100

    test

---
+++ root/index.js
@@ -0,0 +1,13 @@
+const canvas=document.querySelector('canvas')
+
+constc=canvas.getContext('2d')
+
+canvas.width=1024
+canvas.height=576
+
+c.fillStyle='white'
+c.fillRect(0,0,canvas.width,canvas.height)
+const image=new Image()
+
+image.src='./DungeonMap/assets/DungeonLegendsOfRoberto.png'
+console.log(image)
