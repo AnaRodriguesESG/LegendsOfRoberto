68bbeb697e0f85c61631412bfd2a2aa89fe41104
JoaoFerraz190100423 <65223159+JoaoFerraz190100423@users.noreply.github.com>
2022-06-03 14:57:37 +0100

    update

--- root/index.js
+++ root/index.js
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+=======
 const canvas=document.querySelector('canvas')
+>>>>>>> c7aee62422716b6c14c84dfe611aa1be9074f298
 
+<<<<<<< HEAD
+
+=======
 constc=canvas.getContext('2d')
 
 canvas.width=1024
@@ -11,3 +19,5 @@ const image=new Image()
 
 image.src='./DungeonMap/assets/DungeonLegendsOfRoberto.png'
 console.log(image)
+
+>>>>>>> c7aee62422716b6c14c84dfe611aa1be9074f298
