16c0c12f6f7edafb3a43b49be81e6d02f1350153
AnaRodriguesESG <65546790+AnaRodriguesESG@users.noreply.github.com>
2022-06-07 20:42:45 +0100

    Mapa e personagem principal

--- root/index.js
+++ root/index.js
@@ -1,23 +1,289 @@
-<<<<<<< HEAD
 const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
-=======
-const canvas=document.querySelector('canvas')
->>>>>>> c7aee62422716b6c14c84dfe611aa1be9074f298
+const ctx = canvas.getContext('2d');
 
-<<<<<<< HEAD
-
-=======
-constc=canvas.getContext('2d')
+console.log(surfaceCollisions)
 
 canvas.width=1024
-canvas.height=576
+canvas.height=540
 
-c.fillStyle='white'
-c.fillRect(0,0,canvas.width,canvas.height)
-const image=new Image()
+const collisionMap = []
 
-image.src='./DungeonMap/assets/DungeonLegendsOfRoberto.png'
-console.log(image)
+for(let i = 0; i < surfaceCollisions.length; i += 64) {
+    collisionMap.push(surfaceCollisions.slice(i, 64 + i))
+    
+}
 
->>>>>>> c7aee62422716b6c14c84dfe611aa1be9074f298
+class Boundary {
+    constructor(x, y) {
+        this.position = {
+            x: x,
+            y: y
+        }
+        this.width = 64
+        this.height = 64
+    }
+
+    draw() {
+        ctx.fillStyle = "red"
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const boundaries = []
+
+collisionMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if(symbol === 2434)
+        boundaries.push(new Boundary(j * 64 - 2300, i * 64 -2400))
+    })
+})
+
+const test = new Boundary(400, 200)
+
+keyPressed = {
+    w : false,
+    s : false,
+    a : false,
+    d : false
+}
+
+const backgroundImage = new Image()
+backgroundImage.src = 'SurfaceMap.png'
+
+const playerImage = new Image()
+playerImage.src = 'playerStatus/playerWalkDown.png'
+
+console.log(backgroundImage)
+console.log(playerImage)
+
+class Sprite {
+    constructor(x, y, image) {
+        this.position = {
+            x: x,
+            y: y
+        }
+        
+        this.image = image
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.position.x, this.position.y)
+    }
+}
+
+const background = new Sprite(-2300, -2400, backgroundImage)
+
+// Player 
+class Player extends Sprite{
+    
+    
+    constructor(x, y, image, frames = {max:1}) {
+        super(x, y, image);
+        this.frames = {...frames, val:0, elapsed:0}
+
+        this.image.onload = () => {
+            this.width = image.width * 1.5 / 4
+            this.height = image.height * 0.5
+        }
+        
+
+    }
+
+    draw() {
+        ctx.fillStyle = "black"
+        ctx.fillRect(this.position.x + 32, this.position.y + 60, this.width, this.height)
+        ctx.drawImage(this.image, this.frames.val * this.image.width / 4 , 0, this.image.width / 4, this.image.height, this.position.x, this.position.y, this.image.width * 3.5 / 4 , this.image.height * 3.5)
+        
+        if(this.frames.max>1){
+            this.frames.elapsed ++
+        }
+          if(this.frames.elapsed % 10 === 0){
+            if(this.frames.val<this.frames.max-1)this.frames.val ++
+            else this.frames.val=0
+          }  
+        }              
+
+
+    update() {
+        
+    }
+    
+}
+
+const player = new Player(400, 150, playerImage)
+
+//Player Movement
+window.addEventListener('keydown', (event) =>{
+    switch (event.key){
+        case "w":
+            keyPressed.w = true
+            //playerImage.src = 'playerStatus/playerWalkUp.png'
+            break
+        case "s":
+            keyPressed.s = true
+            //playerImage.src = 'playerStatus/playerWalkDown.png'
+            break
+        case "a":
+            keyPressed.a = true
+           // playerImage.src = 'playerStatus/playerWalkLeft.png'
+            break
+        case "d":
+            keyPressed.d = true
+           // playerImage.src = 'playerStatus/playerWalkRight.png'
+            break
+        case " ":
+
+            break
+    }
+})
+
+window.addEventListener('keyup', (event) =>{
+    switch (event.key){
+        case "w":
+            keyPressed.w = false
+            break
+        case "s":
+            keyPressed.s = false
+            break
+        case "a":
+            keyPressed.a = false
+            break
+        case "d":
+            keyPressed.d = false
+            break
+        case " ":
+
+            break
+    }
+})
+
+const movables = [background, ...boundaries]
+
+function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (
+      rectangle1.position.x + 32 + rectangle1.width >= rectangle2.position.x &&
+      rectangle1.position.x + 32 <= rectangle2.position.x + rectangle2.width &&
+      rectangle1.position.y + 60 <= rectangle2.position.y + rectangle2.height &&
+      rectangle1.position.y + 60 + rectangle1.height >= rectangle2.position.y
+    )
+  }
+ 
+
+
+//Função que vai criar animação e desenhar todos os objetos
+function animate() {
+    requestAnimationFrame(animate)
+
+    background.draw()
+    boundaries.forEach((boundary) => {
+        boundary.draw()
+
+        if(rectangularCollision({rectangle1: player, rectangle2: boundary })) {
+            console.log("colision")
+            //console.log(boundary.width, boundary.height)
+            //console.log("position",boundary.position.x, boundary.position.y)
+            //console.log(player.width, player.height)
+        }
+    })
+
+    player.draw()
+
+    let moving = true
+    if(keyPressed.w) {
+
+        for(let i=0 ; i<boundaries.length;i++){
+            const boundary = boundaries[i]
+            if(rectangularCollision({
+                rectangle1:player,
+                rectangle2:{
+                  ... boundary,
+                  position:{
+                    x:boundary.position.x,
+                    y:boundary.position.y + 1.3
+                  }
+                }
+          })) {
+            moving=false
+            break
+          }    
+        }
+             
+        if(moving)
+        movables.forEach(movable => {
+            movable.position.y = movable.position.y + 1.3
+        })
+    }
+    if(keyPressed.s) {
+        for(let i=0 ; i<boundaries.length;i++){
+            const boundary = boundaries[i]
+            if(rectangularCollision({
+                rectangle1:player,
+                rectangle2:{
+                  ... boundary,
+                  position:{
+                    x:boundary.position.x,
+                    y:boundary.position.y - 1.3
+                  }
+                }
+          })) {
+            moving=false
+            break
+          }    
+        }
+             
+        if(moving)
+        movables.forEach(movable => {
+            movable.position.y = movable.position.y - 1.3
+        })
+    }
+    if(keyPressed.a) {
+        for(let i=0 ; i<boundaries.length;i++){
+            const boundary = boundaries[i]
+            if(rectangularCollision({
+                rectangle1:player,
+                rectangle2:{
+                  ... boundary,
+                  position:{
+                    x:boundary.position.x + 1.3,
+                    y:boundary.position.y
+                  }
+                }
+          })) {
+            moving=false
+            break
+          }    
+        }
+             
+        if(moving)
+        movables.forEach(movable => {
+            movable.position.x = movable.position.x + 1.3
+        })
+    }
+    if(keyPressed.d) {
+        for(let i=0 ; i<boundaries.length;i++){
+            const boundary = boundaries[i]
+            if(rectangularCollision({
+                rectangle1:player,
+                rectangle2:{
+                  ... boundary,
+                  position:{
+                    x:boundary.position.x - 1.3,
+                    y:boundary.position.y
+                  }
+                }
+          })) {
+            moving=false
+            break
+          }    
+        }
+             
+        if(moving)
+        movables.forEach(movable => {
+            movable.position.x = movable.position.x - 1.3
+        })
+    }
+    
+}
+animate()
+
+
--- root/utils.js
+++ root/utils.js
@@ -1,4 +1,4 @@
-class AnimatedSprite extends Sprite {
+/*class AnimatedSprite extends Sprite {
 
     static numberFrames;
     static numberFramesPerRow;
@@ -58,4 +58,161 @@ class AnimatedSprite extends Sprite {
         })        
 
     }
+}*/
+
+
+
+class GameObject extends EventTarget {
+
+    constructor(x, y, width, height) {
+        super();
+
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    update() {
+        // a redefinir nas classes derivadas 
+    }
+
+    draw() {
+        // a redefinir nas classes derivadas 
+
+    }
+}
+
+// Um Sprite é um GameObject que tem associado uma imagem 
+class Sprite extends GameObject {
+
+    static imagem;
+
+    constructor(x, y, width, height) {
+        super(x, y, width, height);
+    }
+
+    update() {
+        // a redefinir nas classes derivadas 
+    }
+
+    draw() {
+
+        // BUG Resolvido 
+        // Substituir Sprite por this.constructor
+        /*
+        ctx.drawImage(Sprite.imagem, this.x, this.y, this.width, this.height);
+        */
+
+        ctx.drawImage(this.constructor.imagem, this.x, this.y, this.width, this.height);
+    }
+
+    static load(urlImagem) {
+
+        /*
+        Sprite.imagem = new Image();
+
+        Sprite.imagem.addEventListener( "load", ()=> {
+            window.dispatchEvent( new CustomEvent('assetLoad', { detail: this }))
+        });
+
+        Sprite.imagem.src = urlImagem;
+        */
+
+        // BUG RESOLVIDO
+        // Substituir Sprite por this
+
+        this.imagem = new Image();
+
+        this.imagem.addEventListener("load", () => {
+            window.dispatchEvent(new CustomEvent('assetLoad', { detail: this }))
+        });
+
+        this.imagem.src = urlImagem;
+    }
+}
+
+
+// Um AnimatedSprite é um Sprite com a capacidade de Animação 
+class AnimatedSprite extends Sprite {
+
+    static numberFrames;
+    static numberFramesPerRow;
+    static slice;
+
+    constructor(x, y, width, height) {
+        super(x, y, width, height);
+
+        this.currentFrame = 1;
+
+        this.sx = 0;
+        this.sy = 0;
+    }
+
+    update() {
+
+        // BUG Resolvido 
+        // Substituir AnimatedSprite por this.constructor
+        /*
+        this.currentFrame++;
+
+        if (this.currentFrame > AnimatedSprite.numberFrames)
+            this.currentFrame = 1;
+
+        let deltaX = (this.currentFrame - 1) % AnimatedSprite.numberFramesPerRow;
+        let deltaY = Math.floor((this.currentFrame - 1) / AnimatedSprite.numberFramesPerRow);
+
+        this.sx = deltaX * AnimatedSprite.slice.width;
+        this.sy = deltaY * AnimatedSprite.slice.height;
+        */
+
+        this.currentFrame++;
+
+        if (this.currentFrame > this.constructor.numberFrames)
+            this.currentFrame = 1;
+
+        let deltaX = (this.currentFrame - 1) % this.constructor.numberFramesPerRow;
+        let deltaY = Math.floor((this.currentFrame - 1) / this.constructor.numberFramesPerRow);
+
+        this.sx = deltaX * this.constructor.slice.width;
+        this.sy = deltaY * this.constructor.slice.height;
+    }
+
+
+    draw() {
+
+        // BUG Resolvido 
+        // Substituir AnimatedSprite por this.constructor
+        /*
+        ctx.drawImage(AnimatedSprite.imagem, this.sx, this.sy, AnimatedSprite.slice.width, AnimatedSprite.slice.height,
+            this.x, this.y, this.width, this.height);
+        */
+
+        ctx.drawImage(this.constructor.imagem, this.sx, this.sy, this.constructor.slice.width, this.constructor.slice.height,
+            this.x, this.y, this.width, this.height);
+    }
+
+    static load(urlImagem, numberFrames, numberFramesPerRow) {
+
+        this.imagem = new Image();
+
+        this.imagem.src = urlImagem;
+
+        this.imagem.addEventListener('load', () => {
+
+            this.numberFrames = numberFrames;
+            this.numberFramesPerRow = numberFramesPerRow;
+
+            this.slice = {};
+            this.slice.width = this.imagem.width / numberFramesPerRow;
+
+            let numberRows = Math.ceil(numberFrames / numberFramesPerRow);
+            this.slice.height = this.imagem.height / numberRows;
+
+            window.dispatchEvent(new CustomEvent('assetLoad', { detail: this }));
+
+        });
+
+    }
+
 }
