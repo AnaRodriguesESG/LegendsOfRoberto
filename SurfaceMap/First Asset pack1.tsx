a9533659891bd07813a6793a5b0ccaef9fa54bb7
AnaRodriguesESG <65546790+AnaRodriguesESG@users.noreply.github.com>
2022-06-08 12:33:52 +0100

    Delete SurfaceMap directory

--- SurfaceMap/First Asset pack1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="First Asset pack" tilewidth="16" tileheight="16" margin="10" tilecount="529" columns="23">
- <image source="../assets/First Asset pack.png" width="384" height="384"/>
-</tileset>
